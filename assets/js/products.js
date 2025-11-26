ed9ee76b4fa031a22e98d5c5164ad855f1bff577
gabycrem <gabymacre@gmail.com>
2025-11-26 11:37:47 -0300

    cargar producto al carrito + guardar carrito en localStorage

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,9 +1,12 @@
 const URL = "../assets/data/products.json";
 const container = document.getElementById('list-products');
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let productsData = [];
 
 fetch(URL)
     .then(res => res.json())
     .then(data => {
+        productsData = data;
         container.innerHTML = "";
         data.forEach(product => {
             container.innerHTML += `
@@ -15,7 +18,7 @@ fetch(URL)
                 <p class="card-description">${product.description}</p>
                 <footer class="card-footer">
                     <p class="card-price">$${product.price}</p>
-                    <button class="btn-add-cart">
+                    <button class="btn-add-cart" id="btn-add-cart-${product.id}">
                         <i class='bx bx-cart-add'></i> Añadir
                     </button>
                 </footer>
@@ -24,3 +27,33 @@ fetch(URL)
         });
     })
     .catch(e => console.log(e));
+
+function addToCard(product) {
+    const exists = cart.find(item => item.id === product.id);
+    if (exists) {
+        exists.qty++;
+    } else {
+        cart.push({...product, qty: 1})
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+container.addEventListener('click', (e)=> {
+    if(e.target.closest('.btn-add-cart')){
+        const card = e.target.closest('.card');
+        const id = parseInt(card.dataset.id);
+        const product = productsData.find(p => p.id === id);
+
+        addToCard(product);
+    }
+})
+
+function uploadCart(){
+    console.log("carrito cargado")
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    uploadCart();
+})
+
